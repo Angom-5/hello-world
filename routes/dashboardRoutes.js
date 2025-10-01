5885bbb7e820de4a14aa0806ab0e36cf8ef6b03f
Angom-5 <cangom@student.refactory.academy>
2025-10-01 06:09:03 +0300

    corrected mistakes when authorizing

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,41 +1,39 @@
-// const express = require('express');
-// const router = express.Router();
-// const {
-//   ensureauthenticated,
-//   ensureManager,
-//   ensureAttendant
-// } = require("../middleware/auth");
+const express = require('express');
+const router = express.Router();
+const {
+  ensureauthenticated,
+  ensureManager,
+  ensureAttendant
+} = require("../middleware/auth");
 
 
 router.get('/dashboard', (req, res) => {
-    res.render('userdashboard', { title: 'User Dashboard' });
+    res.render('userDashboard', { title: 'User Dashboard' });
 })
 
 
-// exports.ensureauthenticated = (req, res, next) => {
-//     if (req.isAuthenticated()) {
-//         return next();
-//     }
-//     res.redirect('/login');
-// }
+router.get('/attendant', ensureauthenticated, ensureAttendant, async (req, res) => {
+    res.render('attendantDashboard', { title: 'Attendant Dashboard' });
+})
 
-// router.get('/attendant', ensureAttendant, async (req, res) => {
-//     res.render('attendantDashboard', { title: 'Attendant Dashboard' });
-// })
-
-router.post("/attendant", async (req, res) => {
-  try {
-    const user = new userModel(req.body);
-    await user.save();
-    res.redirect("/login");
-  } catch (error) {
-    res.status(400).send("Sorry something went wrong!");
+router.post(
+  "/attendant",
+  ensureauthenticated,
+  ensureAttendant,
+  async (req, res) => {
+    try {
+      const user = new userModel(req.body);
+      await user.save();
+      res.redirect("/login");
+    } catch (error) {
+      res.status(400).send("Sorry something went wrong!");
+    }
   }
-});
+);
 
 
-// router.get('/manager'), ensureManager, async (req, res) => {
-//     res.render('managerDashboard', { title: 'Manager Dashboard' });
-// }
+router.get('/manager', ensureauthenticated, ensureManager, async (req, res) => {
+    res.render('managerDashboard', { title: 'Manager Dashboard' });
+})
 
-// module.exports = router;
+module.exports = router;
